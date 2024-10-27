59deb9cf508fc3ecad6ee9aef45c49819a8336cb
EmiliaCardenas <142023291+EmiliaCardenas@users.noreply.github.com>
2024-10-27 13:17:52 -0600

    main.cpp

--- main.cpp
+++ main.cpp
@@ -12,9 +12,12 @@ Organizador de cartas (photocards)
 #include <cstring>
 #include <string>
 #include <tuple>
+#include <fstream>
+
 #include "ordenar.h"
 #include "busqueda.h"
 #include "listas.h"
+#include "arbol.h"
 
 /*
 arrayToString - función para hacer el vector a string
@@ -58,9 +61,9 @@ const std::vector<T3>& v3, const std::vector<T4>& v4) {
 }
 
 int main(){
-  int k, j, m;
-  std::string s;
-  List<int> lista;
+  int k, l;
+  std::string s, j, m;
+  List<std::string> lista;
   Buscar<int> buscar;
   Buscar<std::string> buscar2;
 
@@ -72,64 +75,60 @@ int main(){
     << std::endl;
     std::cout << "Si no has hecho el paso 1 antes, hazlo primero" << std::endl;
     std::cout << "Si eliminas un valor, no puedes oprimir 5" << std::endl;
-    std::cout << "\n---Agregar cantidad  1" << std::endl;
-    std::cout << "---Buscar Valor      2" << std::endl;
-    std::cout << "---Cambiar Valor     3" << std::endl;
-    std::cout << "---Eliminar Valor    4" << std::endl;
-    std::cout << "---Ordenamiento      5" << std::endl;
+    std::cout << "\n---Agregar valores  1" << std::endl;
+    std::cout << "---Cambiar Valor      2" << std::endl;
+    std::cout << "---Eliminar Valor     3" << std::endl;
+    std::cout << "---Ordenamiento       4" << std::endl;
     std::cout << "Cual es tu eleccion?" << std::endl;
     std::cin >> k;
 
     if (k == 1){
       // Añadir valores a la lista
-      std::cout << "\nDebes añadir 8 valores numericos" << std::endl;
-      for (int i = 0; i < 8; i++){
-        std::cout << "Valor: " << std::endl;
-        std::cin >> j;
-        lista.insertion(j);
+      std::cout << "\nDebes añadir 8 valores escritos" << std::endl; 
+      std::ifstream archivo("ejemplo.txt");
+      std::string linea;
+      int contador = 0;
+
+      if (archivo.is_open()) {
+        while (getline(archivo, linea) && contador < 8) {
+          std::cout << "Valor: " << linea << std::endl;
+          lista.insertion(linea);
+            contador++;
+        }
       }
+      archivo.close();
       std::cout << lista.toStringForward() << std::endl;
       std::cout << lista.toStringBackward() << std::endl;
     }
 
     if (k == 2){
-      // Busqueda de un valor en la lista
-      std::cout << "\nDa el valor que quieres buscar" << std::endl;
-      std::cin >> m;
-      int encontrar = lista.search(m);
-      std::cout << "\nEncontrar posición de "<< m << std::endl;
-      std::cout << "Se encontro en la posición: " << encontrar << std::endl;
-    }
-
-    if (k == 3){
       // Cambio de un valor
-      std::cout << "\nDa el valor que quieres cambiar" << std::endl;
+      std::cout << "\nDa el valor nuevo a cambiar" << std::endl;
       std::cin >> m;
       std::cout << "En que posición?" << std::endl;
-      std::cin >> j;
-      int encontrar = lista.update(j,m);
+      std::cin >> l;
+      std::string encontrar = lista.update(l,m);
       std::cout << lista.toStringForward() << std::endl;
       std::cout << lista.toStringBackward() << "\n" << std::endl;
     }
 
-    if (k == 4){
+    if (k == 3){
       // Eliminacion de un valor
       std::cout << "\nDa el valor que quieres eliminar" << std::endl;
       std::cin >> m;
-      int encontrar = lista.deleteValue(m);
+      std::string encontrar = lista.deleteValue(m);
       std::cout << lista.toStringForward() << std::endl;
       std::cout << lista.toStringBackward() << "\n" << std::endl;
     }
 
     // Vectores de cada atributo
     std::vector<int> anio = {2000,1999,2000,1998,1997,2001,2000,2000};
-    std::vector<std::string> integrante = {"Jisung","Changbin","Seungmin",
-    "Minho","Chan","Jeongin","Hyunjin","Felix"};
+    std::vector<std::string> integrante = lista.toVector();
     std::vector<std::string> tipo = {"POB","Especial","Basica","POB", "POB",
     "Basica","Especial","Basica"};
-    std::vector<int> cantidad = lista.toVector();
+    std::vector<int> cantidad = {298,85,76,93,98,74,52,67};
 
-    if (k == 5){
+    if (k == 4){
       // Algoritmos de ordenamiento y busqueda
       std::cout << "\nOrden por cantidad: " << std::endl;
       Cuatro<int,int,std::string,std::string> prueba1;
@@ -138,11 +137,6 @@ int main(){
       (cantidad,anio,integrante,tipo);
       printVectors(std::get<0>(resultado), std::get<1>(resultado), 
       std::get<2>(resultado), std::get<3>(resultado));
-
-      std::cout << "\nEn anio encontrar el 2001 en posición 7" 
-      << std::endl;
-      std::cout << "Se encontro en la posición: " << 
-      buscar.busqSecuencialInt(anio,2001)  << std::endl;
 
       std::cout << "\nOrden por nombre: " << std::endl;
       Cuatro<std::string,int,int,std::string> prueba3;
